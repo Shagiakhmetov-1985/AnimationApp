8b07c66ebf3466a07109ae196257054039749dcc
Marat <Shagiakhmetov2499@gmail.com>
2021-04-28 10:14:56 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Marat Shagiakhmetov on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
